6823488dc466ca711f9a08ed59ceb463af59c8ed
shubham <shubhamsinghmor2312@gmail.com>
2024-12-12 23:43:57 +0530

    completed register and login routes for captain

--- backend/controllers/captain.controller.js
+++ backend/controllers/captain.controller.js
@@ -1,1 +1,57 @@
 const CaptainModel=require("../Models/captain.model")
+const captainService=require("../services/captain.services")
+const {validationResult}=require('express-validator')
+
+module.exports.registerCaptain=async(req,res,next)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({erros:errors.array()})
+    }
+
+    const {fullname,email,password,vehicle}=req.body;
+
+    const isCaptainExist=await CaptainModel.findOne({email});
+
+    if(isCaptainExist){
+        return res.status(400).json({message:'catain already exist'})
+    }
+    const hashedPassword= await CaptainModel.hashPassword(password)
+
+    const captain=await captainService.createCaptain({
+            firstname:fullname.firstname,
+            lastname:fullname.lastname,
+            email,
+            password:hashedPassword,
+            color:vehicle.color,
+            plate:vehicle.plate,
+            capacity:vehicle.capacity,
+            vehicleType:vehicle.vehicleType,
+        })
+        
+        const token=captain.generateAuthToken();
+    
+    res.status(201).json({token,captain});
+}
+module.exports.loginCaptain=async (req,res,next)=>{
+    const errors=validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    const {email,password}=req.body;
+
+    const Captain=await CaptainModel.findOne({email}).select('+password');
+
+    if(!Captain){
+        return res.status(401).json({message:'Captain not found'});
+    }
+    const isMatch=await Captain.comparePassword(password)
+
+    if(!isMatch){
+        return res.status(401).json({message:'Password incorrect'})
+    }
+    const token=Captain.generateAuthToken();
+
+    res.cookie('token',token)
+
+    res.status(200).json({token,Captain})
+}
--- backend/routes/captain.routes.js
+++ backend/routes/captain.routes.js
@@ -1,5 +1,20 @@
 const express=require('express')
 const router=express.Router();
+const {body}=require('express-validator')
+const captainController=require('../controllers/captain.controller')
 
+router.post('/register',[
+    body('fullname.firstname').isLength({min:3}).withMessage('First name must be at least 3 charadcters long'),
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min:6}).withMessage('Pasword must be at least 6 characters long'),
+    body('vehicle.color').isLength({min:3}).withMessage('Color must be at least 3 characters long'),
+    body('vehicle.plate').isLength({min:3}).withMessage('Plate must be at least 3 characters long'),
+    body('vehicle.capacity').isInt({min:1}).withMessage('Capacity must be at least 1'),
+    body('vehicle.vehicleType').isIn(['car','motorcycle','auto']).withMessage('Invalid Type'),
+],captainController.registerCaptain)
 
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min:6}).withMessage('Pasword must be at least 6 characters long')
+],captainController.loginCaptain)
 module.exports=router
