87b8b5a472bc897b81474fafdeb53780df8c49ad
shubham <shubhamsinghmor2312@gmail.com>
2024-12-12 19:25:52 +0530

    authentication complete for user

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express=require('express')
 require('dotenv').config();
+const cookieParser=require('cookie-parser')
 const ConnectToDb=require('./db/db.js')
 const userRoutes=require('./routes/user.routes.js')
 const app=express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 
 ConnectToDb()
 app.get('/',(req,res)=>{
