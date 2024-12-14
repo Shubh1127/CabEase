5c9aae3b7e7c96b4f2cc5979b6e78df3d7cdaa59
shubham <shubhamsinghmor2312@gmail.com>
2024-12-14 21:49:52 +0530

    working or frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,24 @@
+
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+// import './App.css'/
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import CaptainSignup from './pages/CaptainSignup'
+import CaptainLogin from './pages/CaptainLogin'
+
+function App() {
+  return (
+   <div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<UserLogin/>}/>
+      <Route path='/signup' element={<UserSignup/>}/>
+      <Route path='/captainlogin' element={<CaptainLogin/>}/>
+      <Route path='/captainsignup' element={<CaptainSignup/>}/>
+    </Routes>
+   </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/pages/UserLogin.jsx
@@ -0,0 +1,15 @@
+const UserLogin = () => {
+  return (
+    <div>
+      <form className="flex flex-col m-3 p-3 ">
+      <h3 className="text-xl">Whats your email?</h3>
+      <input className="border  border-black rounded" type="email" required placeholder="enter your email"></input>
+      <h3 className="text-xl">Password</h3>
+      <input className="border  border-black rounded" type="password"  placeholder="Password" required></input>
+      <button className="bg-black text-white rounded-md mt-2">Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default UserLogin
