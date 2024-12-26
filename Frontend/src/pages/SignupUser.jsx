e39f50c94669914d6a1248b704cd032f9d9c8912
shubham <shubhamsinghmor2312@gmail.com>
2024-12-26 23:17:46 +0530

    working on user sign up component

--- Frontend/src/pages/SignupUser.jsx
+++ Frontend/src/pages/SignupUser.jsx
@@ -1,7 +1,50 @@
+import { auth } from "../../firebaseConfig";
+import { useState } from "react";
+import { createUserWithEmailAndPassword,signInWithPopup,GoogleAuthProvider, sendEmailVerification } from "firebase/auth";
 function SignupUser(){
+    const GoogleProvider=new GoogleAuthProvider();
+    const handleGoogleSignUp=async ()=>{
+        try{
+            const result=await signInWithPopup(auth,GoogleProvider);
+            const user=result.user;
+            console.log(user);
+        }catch(e){
+            console.log(e)
+        }
+    }
+    const handleSubmit=async (e)=>{
+        e.preventDefault();
+        const {name,value}=e.target;
+        setData({
+            ...Data,
+            [name]:value
+        })
+        createUserWithEmailAndPassword(auth,Data.email,Data.password)
+        .then((userCredential)=>{
+            const user=userCredential.user;
+            sendEmailVerification(user).then(()=>{
+                alert('Verfication Link send to Email')
+            })
+            console.log(user);
+        }).catch((e)=>{
+                console.log(e);
+        })
+    }
+    const [Data,setData] = useState({
+        email:"",
+        password:"",
+    });
     return (
-        <div>
-            <h1>Signup User</h1>
+        <div className="p-5">
+            <form className="flex flex-col m-5 p-4" onSubmit={handleSubmit}>
+                <label className="font-semibold" htmlFor="email">Email</label>
+                <input className="border border-black rounded py-2 px-2" type="email" name="email" id="email" value={Data.email} onChange={(e)=>setData({...Data,email:e.target.value})} />
+                <label className="font-semibold" htmlFor="password">Password</label>
+                <input className="border border-black rounded py-2 px-2" type="password" name="password" id="password" value={Data.password} onChange={(e)=>setData({...Data,password:e.target.value})} />
+                <button type="submit" className="w-full bg-black text-white p-2 mt-2 rounded font-semibold">Signup</button>
+            </form>
+            <button className="bg-black w-full rounded text-white p-2 mt-2 font-semibold " onClick={handleGoogleSignUp}>Sign Up with Google</button>
+
         </div>
     )
 }
