238187ed658d3ac6b759c1c9e67bf8d26566cf56
shubham <shubhamsinghmor2312@gmail.com>
2025-02-25 21:37:23 +0530

    implemented google api

--- backend/services/maps.service.js
+++ backend/services/maps.service.js
@@ -1,7 +1,21 @@
-const axios=require('axios');
+const axios = require("axios");
 module.exports.getAddressCoordinates = async (address) => {
-  const response = await axios.get(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.API_KEY}`
-  );
-  return response.data;
-}
+  try{
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${process.env.API_KEY}`
+    );
+    if (response.data.status === "OK") {
+      const location = response.data.results[0].geometry.location;
+      return {
+        ltd: location.lat,
+        lng: location.lng,
+      };
+    } else {
+      console.log(response.data);
+    }
+  }
+  catch(err){
+    console.error(err);
+    throw err;
+  }
+};
