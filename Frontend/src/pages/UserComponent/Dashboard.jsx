05cdcab6a2d563b215901992f044b907432a645f
shubham <shubhamsinghmor2312@gmail.com>
2025-03-12 21:55:20 +0530

    done

--- Frontend/src/pages/UserComponent/Dashboard.jsx
+++ Frontend/src/pages/UserComponent/Dashboard.jsx
@@ -159,7 +159,7 @@ const Dashboard = () => {
             <h5 ref={panelCloseRef} className="text-right text-xl cursor-pointer">
               <i className="ri-arrow-down-wide-line" onClick={() => setPanelOpen(false)}></i>
             </h5>
-            <h4 className="text-2xl font-semibold">Find a Trip</h4>
+            <h4 className="text-3xl font-semibold">Find a Trip</h4>
             <form
               onSubmit={(e) => {
                 submitHandler(e);
