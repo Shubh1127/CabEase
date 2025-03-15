cd0ec7ef1d45bc9dbe960b469996f0628ad1b630
shubham <shubhamsinghmor2312@gmail.com>
2025-03-15 21:54:47 +0530

    done

--- Frontend/src/components/CaptainDetails.jsx
+++ Frontend/src/components/CaptainDetails.jsx
@@ -1,7 +1,10 @@
 import { useCaptainAuth } from "../context/CaptainContext";
+import {socket} from '../context/SocketContext'
 const CaptainDetails=()=>{
   const captainData=JSON.parse(localStorage.getItem('captain'));
-
+  console.log(captainData);
+  const {captain}=useCaptainAuth();
+  socket.emit('join',{captainId:captainData._id});
     return (
         <div >
             <div className="flex items-center justify-between">
