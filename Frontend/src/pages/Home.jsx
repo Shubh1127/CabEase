d46036e8342720232f434d1f20dd30ed6e1f961b
shubham <shubhamsinghmor2312@gmail.com>
2025-05-27 22:56:00 +0530

    done

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="w-full  h-screen">
       <div className=" bg-cover  bg-[url(https://images.unsplash.com/photo-1530652101053-8c0db4fbb5de?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] w-full h-[100vh] flex justify-between flex-col bg-red-400">
-        <img className="w-2/12 m-5" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Uber_logo_2018.png/1200px-Uber_logo_2018.png" alt=""/>
+        <img className="w-2/12 m-4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Uber_logo_2018.png/1200px-Uber_logo_2018.png" alt=""/>
         <div className="bg-white p-4 flex flex-col gap-6">
           <h2 className="font-bold text-2xl">Get Started with Uber</h2>
           {
